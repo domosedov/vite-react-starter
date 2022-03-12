6b83fbe5ef2d7c3520a2a0ed91356afc470d826e
Aleksandr Grigorii <domosedov.dev@gmail.com>
2022-03-12 11:13:41 +0300

    init

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss/tailwind-config').TailwindConfig} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/line-clamp'),
+    require('tailwindcss-radix')(),
+  ],
+}
